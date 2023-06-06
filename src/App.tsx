1fc18664ce93b18c2451350f0b70a75d9ba71180
Victor Nathanael <victornathanael15@gmail.com>
2023-06-06 11:50:04 -0300

    feat: add TaskForm, TaskList and ITask on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import styles from './App.module.css';
+import TaskForm from './components/TaskForm/TaskForm';
+import TaskList from './components/TaskList/TaskList';
+import { ITask } from './interfaces/Task';
 
 function App() {
-    return <div>TODO React</div>;
+    const [taskList, setTaskList] = useState<ITask[]>([]);
+
+    return (
+        <div>
+            <Header />
+            <main className={styles.main}>
+                <div>
+                    <h2>O que você vai fazer?</h2>
+                    <TaskForm
+                        btnText='Criar Tarefa'
+                        taskList={taskList}
+                        setTaskList={setTaskList}
+                    />
+                </div>
+                <div>
+                    <h2>Suas tarefas</h2>
+                    <TaskList />
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
